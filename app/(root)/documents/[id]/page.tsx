144a7488f674d3db93434ad50e6da5317efe1cfb
Jabez H. <jabezhm@gmail.com>
2024-07-21 15:54:51 +0300

    chore: Add lazy loading for images and update LiveblocksProvider resolveUsers function

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,27 @@
 import CollabRoom from '@/components/CollabRoom';
+import { getDocument } from '@/lib/actions/room.actions';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-const Document = () => {
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+
+  if (!clerkUser) {
+    redirect('/sign-in');
+  }
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress
+  });
+
+  if (!room) redirect('/');
+
+  // TODO: check permissions
+
   return (
     <main className='flex w-full flex-col items-center'>
-      <CollabRoom />
+      <CollabRoom roomId={id} roomMetadata={room.metadata} users={[]} />
     </main>
   );
 };
---
+++ components/ActiveCollaborators.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useOthers } from '@liveblocks/react/suspense';
+import Image from 'next/image';
+
+const ActiveCollaborators = () => {
+  const others = useOthers();
+
+  const collaborators = others.map((other) => other.info);
+
+  return (
+    <ul className='collaborators-list'>
+      {collaborators.map(({ id, avatar, name, color }) => (
+        <li key={id} className=''>
+          <Image
+            src={avatar}
+            alt={name}
+            width={100}
+            height={100}
+            className='inline-block size-8 ring-2 ring-dark-100'
+            style={{ border: `3px solid ${color}` }}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ActiveCollaborators;
