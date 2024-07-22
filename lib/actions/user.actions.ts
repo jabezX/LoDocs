543bab47c0016b74913135bdd82d40ed2c16a4d3
Jabez H. <jabezhm@gmail.com>
2024-07-23 00:12:41 +0300

    feat: mention collaborator using the @ symbol

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,8 @@
 'use server';
 
-import { clerkClient } from '@clerk/nextjs/server';
+import { clerkClient, currentUser } from '@clerk/nextjs/server';
 import { parseStringify } from '../utils';
+import { liveblocks } from '../liveblocks';
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
   try {
@@ -21,6 +22,38 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     );
 
     return parseStringify(sortedUsers);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getDocumentUsers = async ({
+  roomId,
+  currentUser,
+  text
+}: {
+  roomId: string;
+  currentUser: string;
+  text: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const users = Object.keys(room.usersAccesses).filter(
+      (email) => email !== currentUser
+    );
+
+    if (text.length) {
+      const lowerCaseText = text.toLowerCase();
+
+      const filteredUsers = users.filter((email) =>
+        email.toLowerCase().includes(lowerCaseText)
+      );
+
+      return parseStringify(filteredUsers);
+    }
+
+    return users;
   } catch (error) {
     console.log(error);
   }
