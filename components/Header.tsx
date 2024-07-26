255f28151804550ee79c480bb16af8ee08c7fd92
Jabez H. <jabezhm@gmail.com>
2024-07-27 00:14:10 +0300

    chore: update logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,18 +8,10 @@ const Header = ({ children, className }: HeaderProps) => {
     <div className={cn('header', className)}>
       <Link href='/' className='md:flex-1'>
         <Image
-          src='/assets/icons/logo.svg'
-          alt='LoDocs logo'
-          width={120}
-          height={32}
-          className='hidden md:block'
-        />
-        <Image
           src='/assets/icons/logo-icon.svg'
           alt='LoDocs logo'
           width={32}
           height={32}
-          className='md:hidden'
         />
       </Link>
       {children}
